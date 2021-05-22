d0332502b63ff3c1e2a38c024242fb8b8d40afaa
Ayush <ayush.kumar20@yahoo.in>
2021-05-23 02:09:16 +0530

    update

--- js/index.js
+++ js/index.js
@@ -7,6 +7,15 @@ function GetClockTime() {
     var min = localdate.getMinutes();
     var sec = localdate.getSeconds();
     var hdeg = hours * 30;
+    if (min > 15 && min < 30) {
+        hdeg = hdeg + 5;
+    }
+    if (min >= 30 && min < 45) {
+        hdeg = hdeg + 15;
+    }
+    if (min >= 45) {
+        hdeg = hdeg + 20;
+    }
     var mindeg = min * 6;
     var secdeg = sec * 6;
     document.getElementById("hour-hand").style = "transform: translateX(-50%) rotate(" + hdeg + "deg);"
